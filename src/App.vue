d450c93b1d93006dd955a1a5d69c8b0fe97776d7
yyh <younghun7513@gmail.com>
2021-12-03 14:59:39 +0900

    init Front-Project

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <!-- <todo-input></todo-input>
+    <todo-list></todo-list> -->
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+
+import TodoHeader from './components/TodoHeader.vue'
+// import TodoInput from './components/TodoInputs.vue'
+// import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+// var my_cmp = {
+//   template: '<div>my component</div>'
+// };
+
+
+// new vue({
+//   el: "app",
+//   components: {
+//     'my-cmp' : my_cmp
+//   }
+
+// });
+
+
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+   'todo-header' : TodoHeader,
+  //  'todo-input' : TodoInput,
+  //  'todo-list' : TodoList,
+   'todo-footer' : TodoFooter
+  }
+}
+
+
+</script>
+
+<style>
+
+body {
+  text-align: center;
+  background-color: azure;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgb(49, 50, 53);
+}
+
+</style>
