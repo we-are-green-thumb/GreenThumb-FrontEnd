c2cc38a078bbfe07bf340a4a4876a81628333d6a
west <real.westsilver@gmail.com>
2021-12-06 11:31:33 +0900

    main component 생성

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template lang="">
+     <!-- <nav-form> -->
+        <div class="search-bar">
+          <select v-model="target" class="btn-front" for="navSearch">
+            <option value="hospital">병원 검색</option>
+            <option value="feed">피드 검색</option>
+          </select>
+          <input
+            class="input-search"
+            type="text"
+            v-model="word"
+            @keyup.enter="search"
+            placeholder
+            id="navSearch"
+          />
+          <button @click="search" class="btn-search" style="outline:0;">
+            <b-icon-search scale="1"></b-icon-search>
+          </button>
+        </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
