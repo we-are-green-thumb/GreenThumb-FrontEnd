25df9dc9388dc8a641f775db61d5db4b0f01f0d3
yyh <younghun7513@gmail.com>
2021-12-08 16:43:12 +0900

    [Fix]vue.config.js

--- src/views/community/IndexCommunity.vue
+++ src/views/community/IndexCommunity.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="aaa">
-    <!-- <plantHeader></plantHeader>
+    <plantHeader></plantHeader>
     <br /><br />
     <plantNav></plantNav>
     <br /><br />
-    <br /><br /> -->
+    <br /><br />
     <hr size="5px">
     <comNav></comNav>
+    <v-btn @click="test"> 클릭</v-btn>
   </div>
 </template>
 <script>
-// import Header from '../../components/Header.vue';
-// import NavigationBar from "../../components/NavigationBar.vue";
+import Header from '../../components/Header.vue';
+import NavigationBar from "../../components/NavigationBar.vue";
 import CommunityNav from "../../components/community/CommunityNav.vue";
 import axios from "axios";
 
 export default {
     components : {
-    // "plantHeader":Header ,
-    // "plantNav":NavigationBar ,
+    "plantHeader":Header ,
+    "plantNav":NavigationBar ,
     "comNav":CommunityNav,
     },
   methods: {
     test() {
-      axios.get("https://reqres.in/api/users?page=2").then(function (response) {
+      axios.get("http://127.0.0.1:80/user/1").then(function (response) {
           console.log(response);
         }).catch(function (error) {
           console.log(error);
