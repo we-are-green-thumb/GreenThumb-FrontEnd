d11f826683ebec61aa84a387435ce702651848c9
west <real.westsilver@gmail.com>
2021-12-16 20:31:01 +0900

    [Add] connect register plant 20211216-20:30

---
+++ src/views/myplant/registermyplant.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="modalp">
+    <h1>식물 등록 컴포넌트</h1>
+    <modalPlantRegister
+      v-if="isModalViewed" @close-modal="isModalViewed = false">
+      <contentPlantRegister msg="Hello Vue in CodeSandbox!"  v-if="isModalViewed" @close-modal="isModalViewed = false" />
+    </modalPlantRegister>
+    <button @click="isModalViewed = true">오픈</button>
+  </div>
+</template>
+
+<script>
+import modalPlantRegister from "../../components/Modal/modalPlantRegister.vue";
+import contentPlantRegister from "../../components/Modal/contentPlantRegister.vue";
+export default {
+  components: {
+    modalPlantRegister,
+    contentPlantRegister,
+  },
+  data() {
+    return {
+      isModalViewed: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#modalp {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
