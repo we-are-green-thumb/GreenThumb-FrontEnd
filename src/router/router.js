6f415209090088dbef529560e7d12af9ef2c2add
Taekyung-Lee <gaetaegoo@gmail.com>
2021-12-17 18:00:52 +0900

    [Merge] 211217-18:00

--- src/router/router.js
+++ src/router/router.js
@@ -9,182 +9,180 @@ import store from "../store"
 
 Vue.use(VueRouter);
 
-
 const rejectAuthuser = (to, from, next) => {
-  if(store.state.isLogin === true){
-    alert("이미 로그인했어요!")
-    next("/")
-  }else {
-    next()
-  }
+    if (store.state.isLogin === true) {
+        alert("이미 로그인했어요!")
+        next("/")
+    } else {
+        next()
+    }
 }
 
 const checkInUser = (to, from, next) => {
-  if(store.state.isLogin === true){
-    next()
-  }else {
-    next("/login")
-    alert("로그인을 부탁드려요!")
-  }
+    if (store.state.isLogin === true) {
+        next()
+    } else {
+        next("/login")
+        alert("로그인을 부탁드려요!")
+    }
 }
 
-
 const IndexMyplant = () => {
-  return import("../views/myplant/Indexmyplant.vue");
+    return import("../views/myplant/Indexmyplant.vue");
 };
 // const modal = () => {
 //   return import("../components/myPlant/Modal.vue")
 // }
 const Detailmyplant = () => {
-  return import("../views/myplant/Detailmyplant.vue");
+    return import("../views/myplant/Detailmyplant.vue");
 };
 const Hospital = () => {
-  return import("../views/hospital/PlantHospital.vue");
+    return import("../views/hospital/PlantHospital.vue");
 };
 
 const editpage = () => {
-  return import("../views/myPage/EditUser.vue")
+    return import("../views/myPage/EditUser.vue")
 }
 const community = () => {
-  return import("../views/community/IndexCommunity.vue")
+    return import("../views/community/IndexCommunity.vue")
 }
 const PostDetail = () => {
-  return import("../views/post/PostDetail.vue")
+    return import("../views/post/PostDetail.vue")
 }
 
 const test = () => {
-  return import("../components/community/test.vue")
+    return import("../components/community/test.vue")
 }
 
 const writepost = () => {
-  return import("../views/post/WritePost.vue")
+    return import("../views/post/WritePost.vue")
 }
 const editpost = () => {
-  return import("../views/post/EditPost.vue")
+    return import("../views/post/EditPost.vue")
 }
 const writecomment = () => {
-  return import("../views/comment/WriteComment.vue")
+    return import("../views/comment/WriteComment.vue")
 }
 const editcomment = () => {
-  return import("../views/comment/EditComment.vue")
+    return import("../views/comment/EditComment.vue")
 }
 const EditPlant = () => {
-  return import('../components/myPlant/editPlant.vue')
+    return import('../components/myPlant/editPlant.vue')
 }
 const routes = [
-  {
-    path: "/",
-    name: "IndexMain",
-    component: IndexMain,
-  },
+    {
+        path     : "/",
+        name     : "IndexMain",
+        component: IndexMain,
+    },
 
-  {
-    path: "/myplant",
-    name: "IndexMyplant",
-    component: IndexMyplant,
-  },
-
-  {
-    path: "/myplant/:userId/:plantId",
-    name: "Detailmyplant",
-    component: Detailmyplant,
-    props: true,
-    children:[
-      {
-        path: "/myplant",
+    {
+        path     : "/myplant",
+        name     : "IndexMyplant",
         component: IndexMyplant,
-      }
-    ]
-    
-  },
-  {
-    path : '/mypage/editpage',
-    name : 'editpage',
-    beforeEnter : checkInUser,
-    component : editpage,
-  }, 
-  {
-    path : '/community',
-    name : 'community',
-    component : community
-  }, 
-  {
-    path: "/hospital",
-    name: "Hospital",
-    component: Hospital,
-  },
-  {
-    path: "/post/writepost",
-    name: "WritePost",
-    component: writepost,
-  },
-  // {
-  //   path: "/modal",
-  //   name: "modal",
-  //   component: modal,
-  // },
-  {
-    path: "/login",
-    name: "loginform",
-    beforeEnter: rejectAuthuser,
-    component: loginform,
-  },
+    },
 
-  {
-    path: "/user2/findpassword",
-    name: "findPassword",
-    component: findPassword,
-  },
+    {
+        path     : "/myplant/:userId/:plantId",
+        name     : "Detailmyplant",
+        component: Detailmyplant,
+        props    : true,
+        children : [
+            {
+                path     : "/myplant",
+                component: IndexMyplant,
+            }
+        ]
 
-  {
-    path: "/user2/register",
-    name: "register",
-    beforeEnter : rejectAuthuser,
-    component: register,
-  },
-  {
-    path: "/post/postDetail/:userId/:postId",
-    name: "PostDetail",
-    component: PostDetail,
-    props: true,
-  },
-  {
-    path: "/post/editpost",
-    name: "EditPost",
-    component: editpost,
-  },
-  {
-    path: "/comment/editcomment",
-    name: "EditComment",
-    component: editcomment,
-  },
-  {
-    path: "/comment/writecomment",
-    name: "WriteComment",
-    component: writecomment,
-  },
-  {
-    path: "/test",
-    name: "test",
-    component: test,
+    },
+    {
+        path       : '/mypage/editpage',
+        name       : 'editpage',
+        beforeEnter: checkInUser,
+        component  : editpage,
+    },
+    {
+        path     : '/community',
+        name     : 'community',
+        component: community
+    },
+    {
+        path     : "/hospital",
+        name     : "Hospital",
+        component: Hospital,
+    },
+    {
+        path     : "/post/writepost",
+        name     : "WritePost",
+        component: writepost,
+    },
+    // {
+    //   path: "/modal",
+    //   name: "modal",
+    //   component: modal,
+    // },
+    {
+        path       : "/login",
+        name       : "loginform",
+        beforeEnter: rejectAuthuser,
+        component  : loginform,
+    },
 
-  },
-  {
-    path: '/plant/:plantId',
-    name: 'EditPlant',
-    component: EditPlant,
-    props: true
-  }
+    {
+        path     : "/user2/findpassword",
+        name     : "findPassword",
+        component: findPassword,
+    },
+
+    {
+        path       : "/user2/register",
+        name       : "register",
+        beforeEnter: rejectAuthuser,
+        component  : register,
+    },
+    {
+        path     : "/post/postDetail/:userId/:postId",
+        name     : "PostDetail",
+        component: PostDetail,
+        props    : true,
+    },
+    {
+        path     : "/post/editpost",
+        name     : "EditPost",
+        component: editpost,
+    },
+    {
+        path     : "/comment/editcomment",
+        name     : "EditComment",
+        component: editcomment,
+    },
+    {
+        path     : "/comment/writecomment",
+        name     : "WriteComment",
+        component: writecomment,
+    },
+    {
+        path     : "/test",
+        name     : "test",
+        component: test,
+
+    },
+    {
+        path     : '/plant/:plantId',
+        name     : 'EditPlant',
+        component: EditPlant,
+        props    : true
+    }
 
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
-  // scrollBehavior (to, from, savedPosition) {
-  //   return { x: 0, y: 0 }
-  // }
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+    // scrollBehavior (to, from, savedPosition) {
+    //   return { x: 0, y: 0 }
+    // }
 });
 
 export default router;
