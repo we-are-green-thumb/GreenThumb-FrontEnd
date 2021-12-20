9276380d7101db697ccac57811e846791d37e3ff
west <real.westsilver@gmail.com>
2021-12-20 11:40:45 +0900

    [Bug]

--- src/components/myPlant/onePlant.vue
+++ src/components/myPlant/onePlant.vue
@@ -12,7 +12,7 @@
         </div>
         <div v-show="contentOwner">
         <router-link
-          :to="{ name: 'EditPlant', params: { plantId: myplant.plantId } }"
+          :to="{ name: 'EditPlant', params: { plantId: this.myplant.plantId,  userId: this.myplant.userId } }"
           ><a>수정하기</a></router-link
         >
         </div>
@@ -34,8 +34,7 @@ export default {
   },
   props: {
     plantId: {
-      type: String,
-      default: "",
+
     },
   },
   computed: {
